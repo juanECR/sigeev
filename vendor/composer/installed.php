09787b8872a8399f7708e3b81d7664e5644ea729
juanECR <rjuanelias2@gmail.com>
2025-08-22 15:11:13 -0500

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sigeev/sistem',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f04b76bee1e29d18a39d2c89307267704d8a3a15',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sigeev/sistem' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f04b76bee1e29d18a39d2c89307267704d8a3a15',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
