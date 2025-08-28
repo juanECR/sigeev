3e38875f6a3ab30a2e7a5a23f1e7962927fd40ab
juanECR <rjuanelias2@gmail.com>
2025-08-28 08:07:13 -0500

    instalmailto

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -21,6 +21,7 @@ class ComposerStaticInit2467712c53098f3abe4e9d0d89038eac
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Http\\Client\\' => 16,
             'PhpOffice\\PhpSpreadsheet\\' => 25,
+            'PHPMailer\\PHPMailer\\' => 20,
         ),
         'M' => 
         array (
@@ -58,6 +59,10 @@ class ComposerStaticInit2467712c53098f3abe4e9d0d89038eac
         'PhpOffice\\PhpSpreadsheet\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Matrix\\' => 
         array (
