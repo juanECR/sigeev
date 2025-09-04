393a16b452fe4d7ba977beba048f8460b3d72d6d
juanECR <rjuanelias2@gmail.com>
2025-09-04 09:39:00 -0500

    Update admin_detalleEvento.js

--- src/view/js/admin_detalleEvento.js
+++ src/view/js/admin_detalleEvento.js
@@ -4,37 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
     listarcategoriasFrm();
     listarOrganizadoresFrm();
 });
-async function listarDetallesEvento() {
-    try {
-        let datos = new FormData();
-        datos.append('sesion',session_session);
-        datos.append('token',token_token);
-        datos.append('id_evento',id_evento);
-
-        let respuesta = await fetch(base_url_server + 'src/control/DetalleEvento.php?tipo=listarDetallesEvento',{
-            method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            body: datos
-        });
-        json = await respuesta.json();
-        if(json.status){
-            let datos = json.contenido;
-            document.querySelector(".page-title").innerHTML = '<i class="fa fa-calendar-check text-primary mr-2"></i> '+datos.titulo+'';
-            document.querySelector("#descripcion_evento").innerHTML =datos.descripcion;
-            document.querySelector(".ubicacion").innerHTML ='<strong>'+datos.ubicacion+'</strong>';
-            document.querySelector(".f_inicio").innerHTML ='<strong>Inicio:</strong> '+datos.fecha_inicio+'';
-            document.querySelector(".f_fin").innerHTML ='<strong>Finaliza:</strong> '+datos.fecha_fin+'';
-            document.querySelector(".organizador").innerHTML ='<strong>'+datos.organizador+'</strong>';
-
-            document.getElementById("#titulo").value = datos.titulo;
-        }else if(json.mensaje == "Error_sesion"){
-            alerta_sesion();
-        }
-    } catch (e) {
-        console.log('Ups ocurrio un error con la funcion' + e);
-    }
-}
 
 async function listarAlgunosParticipantes(){
     try {
@@ -129,6 +98,56 @@ async function listarOrganizadoresFrm(){
         console.log('Ups ocurrio un error con la funcion ' + e);
     }
 }
+
+async function listarDetallesEvento() {
+    let titulo = document.getElementById("titulo");
+    let descripcion = document.getElementById("descripcion");
+    let categoria = document.getElementById("categoria");
+    let fecha_inicio = document.getElementById("fecha_inicio");
+    let fecha_fin = document.getElementById("fecha_fin");
+    let ubicacion = document.getElementById("ubicacion");
+    let organizador = document.getElementById("organizador");
+    let estado = document.getElementById("estado");
+    try {
+        let datos = new FormData();
+        datos.append('sesion',session_session);
+        datos.append('token',token_token);
+        datos.append('id_evento',id_evento);
+
+        let respuesta = await fetch(base_url_server + 'src/control/DetalleEvento.php?tipo=listarDetallesEvento',{
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            body: datos
+        });
+        json = await respuesta.json();
+        if(json.status){
+            let datos = json.contenido;
+            document.querySelector(".page-title").innerHTML = '<i class="fa fa-calendar-check text-primary mr-2"></i> '+datos.titulo+'';
+            document.querySelector("#descripcion_evento").innerHTML =datos.descripcion;
+            document.querySelector(".ubicacion").innerHTML ='<strong>'+datos.ubicacion+'</strong>';
+            document.querySelector(".f_inicio").innerHTML ='<strong>Inicio:</strong> '+datos.fecha_inicio+'';
+            document.querySelector(".f_fin").innerHTML ='<strong>Finaliza:</strong> '+datos.fecha_fin+'';
+            document.querySelector(".organizador").innerHTML ='<strong>'+datos.organizador+'</strong>';
+            titulo.value = datos.titulo;
+            descripcion.value = datos.descripcion;
+           for (let i = 0; i < categoria.options.length; i++) {
+                if(categoria.options[i].value == datos.categoria_evento_id){categoria.options[i].selected = true;}
+            } 
+            
+            fecha_inicio.value = datos.fecha_inicio;
+            fecha_fin.value = datos.fecha_fin;
+            ubicacion.value = datos.ubicacion;
+            organizador.value = datos.organizador_id;
+            estado.value = datos.estado;
+        }else if(json.mensaje == "Error_sesion"){
+            alerta_sesion();
+        }
+    } catch (e) {
+        console.log('Ups ocurrio un error con la funcion' + e);
+    }
+}
+
 async function  editarEvento(){
     try {
         let info = new FormData(frm_editar_evento);
